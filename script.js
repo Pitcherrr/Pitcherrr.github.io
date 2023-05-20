194693ba880dde144f2602d05e134d6fb3616b38
Pitcherrr <tomhkpitcher@gmail.com>
2023-05-20 16:47:55 +1000

    basically done

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const burger = document.querySelector(".nav-icon")
-// gsap.set('body',{overflow:"hidden"})
+// JS File for my website 
+// Author: Tom Pitcher
 
+
+//Mobile pop out menu bar
+const burger = document.querySelector(".nav-icon")
 burger.addEventListener("click", () => {
     if(burger.classList.contains("on")){
         gsap.to(".links", {x: "100%"});
@@ -14,13 +17,14 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("on")
 });
 
-//const videos = gsap.utils.toArray(".iframe");
-const videos = [".funny",".clips"];
 
+//Code for only showing youtube videos when viewing on larger devices
+const videos = [".funny",".clips"];
 if (window.matchMedia('(max-width: 767px)').matches) {
   // Code to execute when the screen width is <= 800px
   console.log('Viewing on a smaller device');
-} else {
+} 
+else {
   // Code to execute when the screen width is > 800px
   gsap.set(videos, {opacity:0});
   videos.forEach((iframe) => {
@@ -28,7 +32,7 @@ if (window.matchMedia('(max-width: 767px)').matches) {
           trigger:iframe,
           start:"top center+=200",
           end:"bottom center",
-          markers: true,
+          markers: false,
           onEnter: () => {
               gsap.to(iframe, {opacity: 1})   
               gsap.to(".clips", {opacity: 1})
@@ -37,33 +41,20 @@ if (window.matchMedia('(max-width: 767px)').matches) {
   });
 }
 
-// window.onscroll = function() {
-//     scrollFunction();
-//   };
 
-//   function scrollFunction() {
-//     var header = document.getElementById(".menu-bar");
-//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//       header.classList.add("scrolled");
-//     } else {
-//       header.classList.remove("scrolled");
-//     }
-//   }
-
-
-
+//code to swich dark mode on, most of the code is also to change the text on the buttons
 function dark_mode() {
-  var element = document.body;
-  element.classList.toggle("mode");
-  var change = document.getElementById("dark-mode-button");
-  if (change.innerHTML == "Dark Mode")
-  {
-    console.log('Changed text')
-    change.innerHTML = "Light Mode";
-  }
-  else {
-    change.innerHTML = "Dark Mode";
-  }
+    var element = document.body;
+    element.classList.toggle("mode");
+    var changes = document.getElementsByClassName("dark-mode-button");
+    for (var i = 0; i < changes.length; i++) {
+        if (changes[i].innerHTML == "Dark Mode") {
+            console.log('Changed text');
+            changes[i].innerHTML = "Light Mode";
+        } else {
+            changes[i].innerHTML = "Dark Mode";
+        }
+    }
 }
 
 
@@ -90,3 +81,26 @@ window.onload = () => {
         })
     }
 }
+
+//code for drop down
+function drop_down(n) {
+    console.log("Drop down for element "+n)
+
+    if (n == 3) {
+        console.log('blog dropdown')
+        var element1 = document.querySelector('.blog-1-text');
+        var element2 = document.querySelector('.blog-1-img');
+        var elements = [element1, elements];
+    }
+    else {
+        var element1 = document.querySelector('.project-' + n + '-text');
+        var element2 = document.querySelector('.project-' + n + '-img');
+        var element3 = document.querySelector('.project-' + n + '-video');
+        var elements = [element1, element2, element3];}
+
+    for (let i=0; i<elements.length; i++) {
+        var element = elements[i];
+        element.classList.toggle("dd");
+        gsap.to(element, {x: "0%"});
+        gsap.fromTo(element, {opacity:0},{opacity:1, delay:0.25, stagger:0})
+}}
